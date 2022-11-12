0e69ccfa47816ba4426e4590889a3a2792810afc
Amdr3B4st0s <113254411+Amdr3B4st0s@users.noreply.github.com>
2022-11-12 12:14:26 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+const ano = 2022;
+let birth = prompt("Digite seu ano de nascimento: ");
+let nome = prompt("Digite seu nome: ");
+let idade = (ano - birth);
+alert(`Olá, seu nome é ${nome} e você tem ${idade} anos.`);
